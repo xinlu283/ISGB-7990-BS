610f9dfd7d2a9b2c9fa43f9e358d65905bfcdc17
xinlu283 <79176251+xinlu283@users.noreply.github.com>
2021-03-02 16:12:34 -0500

    Add files via upload

---
+++ bs_options.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Write a python module, bs_options.py, to price European Options
+
+# In[1]:
+
+
+import math
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# # S=current price
+# # K=strike price
+# # sigma = volatility
+# # t = time to maturity
+# # r = risk free interest rate
+# # d = dividend yield per year
+
+# In[13]:
+
+
+#we impoet the data from the csv doc "data.csv"
+#the first line in data is current price
+
+
+# In[7]:
+
+
+data1 = pd.read_csv('data.csv',header = None)
+data2 = list(data1.split(' '))
+index =['s','k','sigma','t','r','d']
+data = dict(zip(index,data2))
+
+def td1(s,k,r,d,sigma,t):
+    return (math.log(s/k)+(r-d+(sigma^2)/2)*t)/sigma/math.sqrt(t)
+
+def td2(d1,sigma,t):
+    return d1-sigma*math.sqrt(t)
+
+def N(x):
+    return 0.5*(1+math.erf(x/math.sqrt(2)))
+
+d1=td1(data['s'],data['k'],data['r'],data['d'],data['sigma'],data['t'])
+d2=td2(d1,data['sigma'],data['t'])
+
+C = lambda s,d,t,nd1,k,r,nd2:s*math.exp(-d*t)*nd1-k*math.exp(-r*t)*nd2
+print (C(data['s'],data['d'],data['t'],N(d1),data['k'],data['r'],N(d2)))
+
+P = lambda s,d,t,nd1,k,r,nd2:k*math.exp(-r*t)*(-nd2)-s*math.exp(-d*t)*(-nd1)
+print (P(data['s'],data['d'],data['t'],N(d1),data['k'],data['r'],N(d2)))
+
